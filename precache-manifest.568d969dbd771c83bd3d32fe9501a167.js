c7d5cd8bb6bf20602945c92445424ba19bbee448
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-12-26 19:21:44 +0100

    Updates

---
+++ precache-manifest.568d969dbd771c83bd3d32fe9501a167.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6ac0f8cf5f35e85241c3d4776f560b",
+    "url": "/AlbumLibrary-react/index.html"
+  },
+  {
+    "revision": "4ce7f2948ed2ec1e8bc7",
+    "url": "/AlbumLibrary-react/static/css/main.98d0a7cf.chunk.css"
+  },
+  {
+    "revision": "8f68042e3c84405e8da4",
+    "url": "/AlbumLibrary-react/static/js/2.8b9d661e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AlbumLibrary-react/static/js/2.8b9d661e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce7f2948ed2ec1e8bc7",
+    "url": "/AlbumLibrary-react/static/js/main.a8d314d2.chunk.js"
+  },
+  {
+    "revision": "ac04c6d422c390bd2951",
+    "url": "/AlbumLibrary-react/static/js/runtime-main.8cec1070.js"
+  },
+  {
+    "revision": "3be664cd51a0202a208f555ecf27d725",
+    "url": "/AlbumLibrary-react/static/media/default_cover.3be664cd.jpg"
+  },
+  {
+    "revision": "aa99eb4935721c20f9d0e9e3b193d52b",
+    "url": "/AlbumLibrary-react/static/media/logo.aa99eb49.png"
+  }
+]);
